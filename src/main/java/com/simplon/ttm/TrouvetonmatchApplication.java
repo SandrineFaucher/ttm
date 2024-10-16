1be51f143866f328bf23d0cbd9e81e275522a0fd
SFaucher26 <sandrine.faucher@maif.fr>
2024-10-16 18:29:09 +0200

    start

---
+++ src/main/java/com/simplon/ttm/TrouvetonmatchApplication.java
@@ -0,0 +1,13 @@
+package com.simplon.ttm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrouvetonmatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrouvetonmatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplon/ttm/TrouvetonmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplon.ttm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrouvetonmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
