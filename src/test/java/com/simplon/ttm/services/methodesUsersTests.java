96e99b26edbff66d4a015828aa68ab5ab0c67bab
SFaucher26 <sandrine.faucher@maif.fr>
2024-11-06 16:31:06 +0100

    ecriture du test getUserByRole

--- src/test/java/com/simplon/ttm/services/methodesUsersTests.java
+++ src/test/java/com/simplon/ttm/services/methodesUsersTests.java
@@ -5,6 +5,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -127,6 +128,58 @@ public class methodesUsersTests {
 
         assertEquals(userTest, user);
     }
+
+    @Test 
+    void getUserByRole(){
+        //given
+        LocalDate date = LocalDate.parse("2024-11-06");
+        User user = User.builder()
+                .id(1L)
+                .username("User")
+                .password("user123")
+                .role(UserRole.USER)
+                .creationDate(date)
+                .build();
+        User admin = User.builder()
+                .id(1L)
+                .username("Admin")
+                .password("admin123")
+                .role(UserRole.ADMIN)
+                .creationDate(date)
+                .build();
+        User leaderProject = User.builder()
+                .id(1L)
+                .username("LeaderProect")
+                .password("lp123")
+                .role(UserRole.LEADERPROJECT)
+                .creationDate(date)
+                .build();
+        User godparent = User.builder()
+                .id(1L)
+                .username("Parain")
+                .password("parain123")
+                .role(UserRole.GODPARENT)
+                .creationDate(date)
+                .build();
+        //when
+        when(userRepository.findByRole(UserRole.USER)).thenReturn(Optional.of(user));
+        when(userRepository.findByRole(UserRole.ADMIN)).thenReturn(Optional.of(admin));
+        when(userRepository.findByRole(UserRole.LEADERPROJECT)).thenReturn(Optional.of(leaderProject));
+        when(userRepository.findByRole(UserRole.GODPARENT)).thenReturn(Optional.of(godparent));
+
+        //then
+        User userTestRole = userServiceImpl.getUserByRole(UserRole.USER);
+        assertEquals(userTestRole.getUsername(), "User");
+
+        User adminTestRole = userServiceImpl.getUserByRole(UserRole.ADMIN);
+        assertEquals(adminTestRole.getUsername(), "Admin");
+
+        User leaderTestRole  = userServiceImpl.getUserByROle(UserRole.LEADERPROJECT);
+        assertEquals(leaderTestRole.getUsername(), "LeaderProect");
+
+        User godparentTestRole = userServiceImpl.getUserByRole(UserRole.GODPARENT);
+        assertEquals(godparentTestRole.getUsername(), "Parain");
+    };
 }
 
 
