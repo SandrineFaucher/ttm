df1e1d318e59713b221cb2684128fd51d8449b1c
SFaucher26 <sandrine.faucher@maif.fr>
2024-12-16 22:35:25 +0100

    menu dropdown  profil user

--- src/main/webapp/src/page-home/Home.jsx
+++ src/main/webapp/src/page-home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./home.css";
 
 
+
 export default function Home(){
 
     return (
@@ -11,7 +12,9 @@ export default function Home(){
             <p className="text">Le but étant de créer un climat de confiance avec cette personne afin que vous puissiez vous tourner vers elle lorsque vous avez des soucis ou des besoins.</p>
             <p className="text">Ce parrain / marraine peut être un chef d'entreprise, ou d'un cadre dirigeant ayant de l'expérience et un réseau important pour faire appel à des partenaires experts dans vos besoins.</p>
 
-            <button>Je crée mon compte</button>
+            <button>Je me connecte</button>
+
+
 
       </>
     )
--- src/main/webapp/src/page-messagerie/Messagerie.jsx
+++ src/main/webapp/src/page-messagerie/Messagerie.jsx
@@ -3,8 +3,9 @@ import React from "react";
 export default function Messagerie(){
 
     return (
-
+            <>
             <h1>Page Messagerie</h1>
 
+            </>
     )
 }
