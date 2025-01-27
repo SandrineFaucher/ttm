129c6e881d4ffac4af65598c8a51fce93b3af846
SFaucher26 <sandrine.faucher@maif.fr>
2025-01-27 21:52:43 +0100

    methode ppost d'un secteur

--- src/main/java/com/simplon/ttm/services/impl/UserServiceImpl.java
+++ src/main/java/com/simplon/ttm/services/impl/UserServiceImpl.java
@@ -5,13 +5,10 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.simplon.ttm.dto.LoginDto;
 import com.simplon.ttm.dto.RegisterDto;
 import com.simplon.ttm.models.User;
 import com.simplon.ttm.models.UserRole;
