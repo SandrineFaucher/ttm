2921f511506a5c6661d323e018311d37a730f686
SFaucher26 <sandrine.faucher@maif.fr>
2025-02-05 11:26:29 +0100

    migration des donnÃees secteurs et accompagnement

---
+++ src/main/resources/db/migration/data/V1.1__INSERT_DATAS.sql
@@ -0,0 +1,25 @@
+INSERT INTO sector (id, content)
+VALUES (4,'Activités de services administratifs et de soutien'),
+       (5,'Activités spécialisées, scientifiques et techniques'),
+       (6,'Agriculture, sylviculture, pêche'),
+       (7,'Arts, spectacles et activités récréatives'),
+       (8,'Commerce et réparation'),
+       (9,'Construction-BTP'),
+       (10,'Enseignement'),
+       (11,'Hôtels, cafés et restaurants'),
+       (12,'Industrie'),
+       (13,'Information et communication'),
+       (14,'Production et distribution d’eau, assainissement, gestion des déchets et dépollution'),
+       (15,'Production et distribution d’électricité, de gaz, de vapeur d’air conditionné'),
+       (16,'Santé humaine et action sociale'),
+       (17,'Services aux entreprises'),
+       (18,'Services aux particuliers'),
+       (19,'Transports');
+
+INSERT INTO accompaniement (id, content)
+VALUES (4,'Ressources humaines'),
+       (5,'Finance / Comptabilité'),
+       (6,'Juridique'),
+       (7,'Informatique'),
+       (8,'Commercial / Communication');
+
