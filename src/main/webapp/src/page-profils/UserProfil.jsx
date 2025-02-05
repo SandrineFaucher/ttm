20e0b11c8f4d7fe2e14e0aa6e70f00d503435516
SFaucher26 <sandrine.faucher@maif.fr>
2025-02-05 13:31:24 +0100

    recuperation page profil

--- src/main/webapp/src/page-profils/UserProfil.jsx
+++ src/main/webapp/src/page-profils/UserProfil.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import UserUpdate from "./UserUpdate.jsx";
 import UpdatePassword from "./UpdatePassword.jsx";
+import FormProfil from "./FormProfil.jsx";
+import "./userProfil.css";
 
 
 export default function UserProfil(){
 
     return (
             <>
-            <h1>Page Profil User</h1>
-                <div className="container-update">
-                <UserUpdate/>
-                <UpdatePassword/>
+            <h1>Mon Profil </h1>
+                <div className="container">
+                    <div className="block">
+                        <h2>Modifier mes données</h2>
+                        <div className="component">
+                            <UserUpdate/>
+                        </div>
+                        <div className="component">
+                            <UpdatePassword/>
+                        </div>
+                    </div>
+                    <div className="block">
+                        <h2>Remplir mon profil</h2>
+                        <FormProfil/>
+                    </div>
                 </div>
             </>
     )
