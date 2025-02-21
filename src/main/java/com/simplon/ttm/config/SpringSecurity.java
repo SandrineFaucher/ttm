26a40b6281abb05fbb017e43d070089e0d756f9b
SFaucher26 <sandrine.faucher@maif.fr>
2025-02-21 14:34:23 +0100

    affichage previewCard for updateProfil

--- src/main/java/com/simplon/ttm/config/SpringSecurity.java
+++ src/main/java/com/simplon/ttm/config/SpringSecurity.java
@@ -45,7 +45,7 @@ public class SpringSecurity {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/","/login", "/authenticate").permitAll()
+                        .requestMatchers("/","/login", "/authenticate", "/uploads/**").permitAll()
                         .requestMatchers(HttpMethod.PUT, "/userUpdate/**").authenticated()
                         .anyRequest().authenticated()
                 )
