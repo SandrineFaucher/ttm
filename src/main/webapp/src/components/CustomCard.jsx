907985bd6a451ac990ce8c66c7b8a1c43da096bb
SFaucher26 <sandrine.faucher@maif.fr>
2025-02-21 14:34:23 +0100

    affichage previewCard for updateProfil

--- src/main/webapp/src/components/CustomCard.jsx
+++ src/main/webapp/src/components/CustomCard.jsx
@@ -1,20 +1,36 @@
 import "./customCard.css"
 import PropTypes from "prop-types";
 
-const CustomCard =({ title, sectors, accompaniements, city, department, region, description}) =>{
+const CustomCard =({ title, image, availability, sectors, accompaniements, city, department, region, description}) =>{
+
+    console.log(image);
     return (
         <div className="body-card">
             <p className="location">{region} {city}{department}</p>
             <h2 className="pseudo-user">{title}</h2>
-            <img alt="photo-profil"/>
+            <img className="profil_image" src={image} alt="photo-profil"/>
+            <div className="small-containers">
+            <h5>Disponibilités :</h5>
+            <p>{availability}</p>
+            </div>
+            <div className="small-containers">
+            <h5>Secteurs / réseaux :</h5>
             <p>{sectors}</p>
+            </div>
+            <div className="small-containers">
+            <h5>Accompagnements :</h5>
             <p>{accompaniements}</p>
+            </div>
+            <div className="small-containers">
+            <h5>Le petit mot en plus :</h5>
             <p>{description}</p>
+            </div>
         </div>
     )
 };
 CustomCard.prototype = {
     title : PropTypes.string,
+    availability: PropTypes.string,
     sectors: PropTypes.string,
     accompaniements: PropTypes.string,
     city: PropTypes.string,
--- src/main/webapp/src/page-profils/PreviewCard.jsx
+++ src/main/webapp/src/page-profils/PreviewCard.jsx
@@ -2,18 +2,37 @@ import CustomCard from "../components/CustomCard.jsx";
 import React, {useContext} from "react";
 import {AuthContext} from "../context/AuthContext.jsx";
 
+
 export default function PreviewCard() {
+    /**
+     * Récupération des variables en utilisant le contexte
+     */
     const {auth} = useContext(AuthContext);
     // Vérifie si auth est null ou undefined
     if (!auth) {
         return <p>Chargement...</p>;
     }
+    const sectorsContent = auth?.profil?.sectors.map(s =>s.content);
+    const accompaniementContent = auth?.profil?.accompaniements.map(s => s.content);
+
+    const baseUrl = "http://localhost:8080/";
+    const imagePath = auth?.profil.image;
+    const defaultImage = "uploads/profil_images/default_profile_picture.png";
+    const imageUrl = imagePath ? `${baseUrl}${imagePath}` : `${baseUrl}${defaultImage}`;
+
 
     return (
         <>
         <CustomCard
         title={auth.username}
-
+        region={auth.profil.region}
+        department={auth.profil.department}
+        city={auth.profil.city}
+        image={imageUrl}
+        sectors={sectorsContent}
+        accompaniements={accompaniementContent}
+        availability={auth.profil.availability}
+        description={auth.profil.content}
         />
         </>
     );
