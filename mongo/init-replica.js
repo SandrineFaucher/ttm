b3cf563bdc108370f8f2bc97603bab5c619a8dee
SFaucher26 <sandrine.faucher@maif.fr>
2025-06-12 19:32:26 +0200

    feat: modif initMongo

--- mongo/init-replica.js
+++ mongo/init-replica.js
@@ -1,5 +1,4 @@
 if(rs.status().includes("no replset config has been received")) {
 
   rs.initiate({_id: 'rs0', members: [{ _id: 0, host: 'localhost:27017' }]})
-
 }
--- mongo/initMongo.Dockerfile
+++ mongo/initMongo.Dockerfile
@@ -2,4 +2,5 @@ FROM mongo:6
 
 COPY init-replica.js /docker-entrypoint-initdb.d/init-replica.js
 
+
 RUN chmod +x /docker-entrypoint-initdb.d/init-replica.js
