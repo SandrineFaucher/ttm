c83452d49f859e6e2742a85bcaca72a6320e4d83
SFaucher26 <sandrine.faucher@maif.fr>
2025-06-13 15:59:37 +0200

    feat: fix message

--- src/main/java/com/simplon/ttm/config/WebSocketConfig.java
+++ src/main/java/com/simplon/ttm/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns(API_HOST.replace("https://", "wss://").replace("http://", "ws://"));
+                .setAllowedOriginPatterns(API_HOST);
     }
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
