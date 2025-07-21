a6e2b495b6cb8ec3de411973f8c38d410331a90d
SFaucher26 <sandrine.faucher@maif.fr>
2025-07-21 10:59:24 +0200

    feat: suppression fichier non utilisé

--- src/main/java/com/simplon/ttm/config/MongoReplicaInitializer.java
+++ src/main/java/com/simplon/ttm/config/MongoReplicaInitializer.java
@@ -20,40 +20,7 @@ public class MongoReplicaInitializer {
     @Value("${spring.data.mongodb.uri}")
     private String MONGO_HOST;
 
-    //    @Bean
-    //    public CommandLineRunner initReplicaSetIfNeeded() {
-    //        return args -> {
-    //            String host = MONGO_HOST.replaceFirst("[\\?|&]replicaSet=[a-zA-Z0-9]+&?", "");
-    //            try (MongoClient mongoClient = MongoClients.create(host)) {
-    //                MongoDatabase adminDb = mongoClient.getDatabase("admin");
-    //
-    //                // Je vérifie si le replica est déjà initialisé
-    //                try {
-    //                    adminDb.runCommand(new Document("replSetGetStatus", 1));
-    //                    System.out.println("Replica Set déjà initialisé, rien à faire.");
-    //                    return; // on arrête là
-    //                } catch (MongoCommandException e) {
-    //                    if (e.getErrorCode() == 94) {
-    //                        System.out.println("Replica Set non initialisé, on l'initialise...");
-    //                        // Code d’erreur 94 = NotYetInitialized
-    //                    } else {
-    //                        throw e; // autre erreur → on la remonte
-    //                    }
-    //                }
-    //                // S'il ne l'est pas je l'initialise
-    //                Document config = new Document("_id", "rs0")
-    //                        .append("members", List.of(
-    //                                new Document("_id", 0).append("host", host.replace("mongodb://", ""))
-    //                        ));
-    //
-    //                adminDb.runCommand(new Document("replSetInitiate", config));
-    //                System.out.println("Replica Set initialisé avec succès !");
-    //            } catch (Exception e) {
-    //                System.err.println("Erreur pendant l'initialisation du replica set : " + e.getMessage());
-    //            }
-    //        };
-    //    }
-    //}
+
     @Bean
     public CommandLineRunner initReplicaSetIfNeeded() {
         return args -> {
